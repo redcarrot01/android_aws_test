e728e2300d861b72790839e5800cf9db9fd9a0bf
redcarrot01 <redccc9010@gmail.com>
2020-12-04 11:17:20 +0900

    1204 11시 commit , camera 완성

--- app/src/main/java/com/example/amplifytest/MainActivity.java
+++ app/src/main/java/com/example/amplifytest/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.amplifytest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                startActivity(intent); //액티비티 이동 해주는 구문
+            }
+        });
     }
+
 }
-/////// xcxx hh
-//
